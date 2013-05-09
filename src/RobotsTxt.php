741b9f10750eff77419c42b499c776918e574564
David Persson <davidpersson@gmx.de>
2013-05-09 14:35:33 +0200

    Readding RobotsTxt class for BC.

---
+++ src/RobotsTxt.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace indexed;
+
+/**
+ * @deprecated
+ */
+class RobotsTxt extends Robots {}
+
+trigger_error('The RobotsTxt class is deprecated use Robots instead.', E_USER_DEPRECATED);
+
+?>
